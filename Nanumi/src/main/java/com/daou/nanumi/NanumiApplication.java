e62cd6dab79aa1c7b6500b17e55bce6ac2920930
tmddn1304 <tmddn1304@gmail.com>
2020-05-21 17:21:23 +0900

    [+] 나누미 초기세팅

---
+++ Nanumi/src/main/java/com/daou/nanumi/NanumiApplication.java
@@ -0,0 +1,13 @@
+package com.daou.nanumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanumiApplication.class, args);
+	}
+
+}
