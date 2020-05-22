cd2284e08a4ece155817561ebcbfdc249eb03506
박경호 <pkh879@gmail.com>
2020-05-22 16:47:12 +0900

    멀티 모듈 환경설정 - 모듈 간 의존관계 설정 2

--- Nanumi/module-web/src/main/java/com/daou/NanumiWebApplication.java
+++ Nanumi/module-web/src/main/java/com/daou/NanumiWebApplication.java
@@ -2,11 +2,8 @@ package com.daou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class NanumiWebApplication {
     public static void main(String[] args) {
