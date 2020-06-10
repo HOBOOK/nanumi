7dcfed7c8b54ef76ea7f30fbe07d8c0ae07fd73a
tmddn1304 <tmddn1304@gmail.com>
2020-06-10 15:07:05 +0900

    [+UPDATE] logout시 Refresh Token도 제거하도록 수정

--- Front/admin_nanumi/src/store/index.js
+++ Front/admin_nanumi/src/store/index.js
@@ -25,6 +25,7 @@ export default new Vuex.Store({
     logout: function(options){
       options.commit('setToken', null)
       localStorage.removeItem("token");
+      localStorage.removeItem("refreshToken")
     },
     updateToken: function(options, token){
       options.commit('setToken', token)
